3cbeb88ac7824c930b3dcc50d8d7c2320fb851a5
SangryeolPark <tkdfuf0923@gmail.com>
2023-05-25 18:03:22 +0900

    feat: Visual 작업 중

---
+++ js/visual.js
@@ -0,0 +1,34 @@
+window.addEventListener("load", function (event) {
+    const swVisualWrap = document.querySelector(".sw-visual .swiper-wrapper");
+
+    const xhr = new XMLHttpRequest();
+    xhr.addEventListener("readystatechange", function (event) {
+        let req = event.target;
+        if (req.readyState === XMLHttpRequest.DONE) {
+            let data = JSON.parse(req.response);
+            makeVisualHTML(data);
+        }
+    });
+    xhr.open("GET", "data/visualdata.json");
+    xhr.send();
+
+    function makeVisualHTML(_data) {
+        let html = ``;
+        _data.img.forEach((data) => {
+            let temp = `<div class="swiper-slide" style="background-image: url(images/${data})"></div>`;
+            html += temp;
+        });
+
+        swVisualWrap.innerHTML = html;
+
+        const swVisual = new Swiper(".sw-visual", {
+            loop: true,
+            effect: "fade",
+            speed: 800,
+            autoplay: {
+                delay: 2500,
+                disableOnInteraction: false,
+            },
+        });
+    }
+});
