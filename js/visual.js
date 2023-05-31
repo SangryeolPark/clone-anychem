a20afdfbfff38bb618f6b96acfa0957d88bd297b
SangryeolPark <tkdfuf0923@gmail.com>
2023-05-31 16:38:27 +0900

    feat: Visual 작업 완료 및 Header 수정

--- js/visual.js
+++ js/visual.js
@@ -1,45 +1,38 @@
 window.addEventListener('load', function (event) {
-  const swVisualWrap = document.querySelector('.sw-visual .swiper-wrapper');
+	const swVisualWrap = document.querySelector('.sw-visual .swiper-wrapper');
 
-  // const xhr = new XMLHttpRequest();
-  // xhr.addEventListener('readystatechange', function (event) {
-  //   let req = event.target;
-  //   if (req.readyState === XMLHttpRequest.DONE) {
-  //     let data = JSON.parse(req.response);
-  //     makeVisualHTML(data);
-  //   }
-  // });
-  // xhr.open('GET', 'data/visualdata.json');
-  // xhr.send();
-  fetch('data/visualdata.json')
-    .then((res) => res.json())
-    .then((result) => makeVisualHTML(result))
-    .catch((err) => console.error(err));
+	fetch('data/visualdata.json')
+		.then((res) => res.json())
+		.then((result) => makeVisualHTML(result))
+		.catch((err) => console.error(err));
 
-  function makeVisualHTML(_data) {
-    let html = ``;
-    _data.img.forEach((data) => {
-      let temp = `<div class="swiper-slide" style="background-image: url(images/${data})"></div>`;
-      html += temp;
-    });
+	function makeVisualHTML(_data) {
+		let html = ``;
+		_data.img.forEach((data) => {
+			let temp = `<div class="swiper-slide" style="background-image: url(images/${data})"></div>`;
+			html += temp;
+		});
 
-    swVisualWrap.innerHTML = html;
+		swVisualWrap.innerHTML = html;
 
-    const swVisual = new Swiper('.sw-visual', {
-      loop: true,
-      effect: 'fade',
-      speed: 800,
-      autoplay: {
-        delay: 2500,
-        disableOnInteraction: false,
-      },
-      pagination: {
-        el: '.sw-visual-pg',
-        pagination: {
-          el: '.sw-visual-pg',
-          clickable: true,
-        },
-      },
-    });
-  }
+		const swVisual = new Swiper('.sw-visual', {
+			loop: true,
+			effect: 'fade',
+			speed: 800,
+			autoplay: {
+				delay: 2500,
+				disableOnInteraction: false,
+			},
+			pagination: {
+				el: '.sw-visual-pg',
+				pagination: {
+					el: '.sw-visual-pg',
+					clickable: true,
+				},
+			},
+		});
+
+		const swVisualBullets = document.querySelectorAll('.sw-visual-pg .swiper-pagination-bullet');
+		swVisualBullets.forEach((item, index) => (item.innerHTML = `<em>${index < 9 ? '0' : ''}${index + 1}</em>`));
+	}
 });
